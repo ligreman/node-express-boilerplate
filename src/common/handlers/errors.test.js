9df545da6cd8245e2dc3ba6d05cf538538d9a755
Ligre <ligreman@users.noreply.github.com>
2019-02-25 21:38:30 +0100

    Gulp

--- src/common/handlers/errors.test.js
+++ src/common/handlers/errors.test.js
@@ -1,10 +1,4 @@
-require('module-alias/register');
 const assert = require('assert');
-const request = require('supertest');
-
-// Indico que estoy en modo test
-global.testModeExecution = true;
-const app = require('@root/app');
 
 // Para saber por qué mocha (o node) se queda "colgado" al terminar los tests, o lo que sea
 const wtf = require('wtfnode');
@@ -13,9 +7,6 @@ const wtf = require('wtfnode');
 after(function () {
     // Para saber qué cosas quedan pendientes tras la ejecución de los tests (si se queda colgado mocha)
     wtf.dump();
-
-    // Cierro el servidor
-    app.get('listeningServer').close();
 });
 
 describe('Array', function () {
@@ -23,30 +14,5 @@ describe('Array', function () {
         it('should return -1 when the value is not present', function () {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
-    });
-});
-
-describe('GET /api', function () {
-    it('responds with json method 1', function (done) {
-        request(app)
-            .get('/api')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .end(function (err, res) {
-                if (err) {
-                    // Falla el test. También podría hacer un throw
-                    return done(err);
-                }
-                done();
-            });
-    });
-
-    it('responds with json method 2', function (done) {
-        request(app)
-            .get('/api')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, done);
     });
 });
